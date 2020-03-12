3b94da2c8c2627a615c1304483fc5680868ec179
GoodField55 <goodfield055@gmail.com>
2020-03-12 08:19:08 +0000

    change clone address ssh -> http

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -5,7 +5,7 @@ rsync -av ./ ~/catkin_ws/src/pimouse_run_corridor/
 
 #pimouse_ros を git clone でワークスペースに持ってくる
 cd ~/catkin_ws/src/
-git clone --depth=1 git@github.com:GoodField55/pimouse_ros.git
+git clone --depth=1 https://github.com/GoodField55/pimouse_ros.git
           #↑depth=1を指定すると最新のものだけをクローンできる
 
 cd ~/catkin_ws
